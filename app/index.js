ae140b8910d494566cc665cb05ea808d2d37e555
redveronika <voronovaveronika@gmail.com>
2017-12-19 17:26:31 +0300

    Подключила redux, react-router

--- app/index.js
+++ app/index.js
@@ -1,6 +1,30 @@
 import React from 'react'
 import {render} from 'react-dom'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import reducer from './reducers';
 
 import Main from '../app/blocks/main/main.js';
 
-render(<Main />, document.getElementById('app'));
+const store = createStore(reducer, composeWithDevTools());
+
+const component = () => {
+    render(
+        <Provider store={store}>
+            <Router>
+                <Route path="/" component={Main} />
+            </Router>
+        </Provider>,
+        document.getElementById('app'),
+    );
+};
+
+component();
+
+if (module.hot) {
+    module.hot.accept(() => {
+        component();
+    });
+}
