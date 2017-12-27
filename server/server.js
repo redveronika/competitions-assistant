a3cccc0d17ad827f50fa47b7cfcae4ede7f0ad00
redveronika <voronovaveronika@gmail.com>
2017-12-27 18:07:26 +0300

    Убрала плагин очистик директории из конфига webpack, поправила путь к индексу, добавила порт в переменную среды

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
-const bodyParser= require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const webpackDevHelper = require('./server.dev');
 
 const isDev = process.env.NODE_ENV !== 'production';
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 if (isDev) {
     webpackDevHelper.useWebpackMiddleware(app);
@@ -16,14 +17,12 @@ if (isDev) {
     app.use(express.static('dist'));
 }
 
-
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/dist/index.html`);
+    res.sendFile(path.resolve(__dirname, '../dist/index.html'));
 });
 
-MongoClient.connect(process.env.MONGO_DB_URI_COMPETITIONS_ASSISTANT, (err, database) => {
-    app.listen(4444, () => {
-        console.log('listening on 4444');
-
+MongoClient.connect(process.env.MONGO_DB_URI, () => {
+    app.listen(process.env.PORT || 5555, () => {
+        console.log(`listening on ${process.env.PORT}`);
     });
 });
--- webpack/shared.config.js
+++ webpack/shared.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
+
 
 const config = {
     output: {
@@ -16,7 +16,6 @@ const config = {
         }],
     },
     plugins: [
-        new CleanWebpackPlugin(['../dist'], { allowExternal: true }),
         new HtmlPlugin({
             filename: 'index.html',
             template: 'client/index.html',
